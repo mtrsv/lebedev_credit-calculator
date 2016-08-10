5e61e205353a7936ef33d64cfa8a3a262a78ab5e
Ilia Matrosov <ilia.matrosov@yahoo.com>
2016-08-10 12:06:25 +0500

    Works in chrome, opera, firefox

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,10 @@
             progressElem = options.progress || null,
             progressElemRight = options.progressRight || null,
             bubbleElem = options.bubble || null,
+            clickAreas = options.clickAreas || [],
             max = options.max || 100,
             min = options.min || 0,
+            step = options.step || 1,
             currentPosition = options.currentPosition || 0,
             coef = (sliderElem.offsetWidth - thumbElem.offsetWidth)/(max - min),
             newLeft,
@@ -24,9 +26,15 @@
 
         setValue(currentPosition);
 
-        sliderElem.addEventListener("mousedown",onMouseDown);
-        progressElem.addEventListener("mousedown",onMouseDown);
-        thumbElem.addEventListener("mousedown",onMouseDown);
+        clickAreas.push(sliderElem);
+        clickAreas.push(progressElem);
+        clickAreas.push(thumbElem);
+        clickAreas.push(bubbleElem);
+        clickAreas.forEach(addClickListener);
+
+        function addClickListener(area){
+            if (area) area.addEventListener("mousedown",onMouseDown);
+        }
 
         document.addEventListener("mousemove",onMouseMove);
         document.addEventListener("mouseup",onMouseUP);
@@ -37,6 +45,7 @@
         };
 
         function onMouseMove(e) {
+            e.preventDefault();
             if (!isDrag) return;
 
             var sliderCoords = getCoords(sliderElem);
@@ -96,7 +105,8 @@
         }
 
         function convertCoordinateToValue(coordinate){
-            return Math.round(newLeft/coef) + min;
+            var value = Math.round(newLeft/coef) + min;
+            return Math.round(value/step)*step;
         }
 
         function convertValueToCoordinate(value){
@@ -172,25 +182,38 @@
             resultElement = options.resultElement,
             depositElement = options.depositElement,
             bubbleElement = options.bubbleElement,
+            buttonsContainer = options.buttonsContainer,
             yearProfit = options.yearProfit || 146, // profit in percents/year
-            periods = options.options || [1, 3, 6], //array of periods available
-            currentPeriod;
+            periods = options.periods || [1, 3, 6], //array of periods available
+            periodName = options.periodName || "мес",
+            currentPeriod,
+            currentValue;
 
-        sliderElement.addEventListener("slide",onChange.bind(this));
+        init();
+
+        function init(){
+            generateButtons();
+            currentValue = slider.value;
+            updateTextFields();
+
+            sliderElement.addEventListener("slide",onChange.bind(this));
+            buttonsContainer.addEventListener("click",onButtonsClick);
+        }
 
         function onChange(e){
             //console.log(e);
-            updateTextFields(e.detail);
+            currentValue = e.detail;
+            updateTextFields();
         }
 
-        function updateTextFields(value){
-            if (resultElement) resultElement.innerHTML = getValueInText(value);
-            if (depositElement) depositElement.innerHTML = getValueInText(value + calculateProfit());
-            if (bubbleElement) bubbleElement.innerHTML = getValueInText(value);
+        function updateTextFields(){
+            if (resultElement) resultElement.innerHTML = getValueInText(currentValue + calculateProfit());
+            if (depositElement) depositElement.innerHTML = getValueInText(currentValue);
+            if (bubbleElement) bubbleElement.innerHTML = getValueInText(currentValue);
         }
 
         function calculateProfit(){
-            return 1000;
+            return Math.floor(currentValue * currentPeriod * yearProfit/100/12);
         }
 
         function getValueInText(value){
@@ -199,6 +222,32 @@
                     currency: "RUB",
                     minimumFractionDigits: 0
                 });
+        }
+
+        function generateButtons(){
+            periods.forEach(function(buttonValue, i){
+                var button = document.createElement("button");
+                button.classList.add("d-c-period-button");
+                button.dataset.period = buttonValue;
+                if (i == 0) {
+                    button.classList.add("d-c-period-button--active");
+                    currentPeriod = buttonValue;
+                }
+                button.textContent = buttonValue + " " + periodName;
+                buttonsContainer.appendChild(button);
+            });
+        }
+
+        function onButtonsClick(e){
+            var button = e.target.closest(".d-c-period-button");
+            if (!button) return;
+            currentPeriod = button.dataset.period;
+            var buttons = buttonsContainer.querySelectorAll(".d-c-period-button");
+            Array.prototype.forEach.call(buttons,function(button){
+                button.classList.remove("d-c-period-button--active");
+            });
+            button.classList.add("d-c-period-button--active");
+            updateTextFields();
         }
     }
 
@@ -210,9 +259,16 @@
         progress: calculatorElement.querySelector(".d-c-slider__progress"),
         progressRight: calculatorElement.querySelector(".d-c-slider__bright-box"),
         bubble: calculatorElement.querySelector(".d-c-slider__bubble"),
+        clickAreas:[
+            calculatorElement.querySelector(".d-c-slider__background"),
+            calculatorElement.querySelector(".d-c-slider__bright-box"),
+            calculatorElement.querySelector(".d-c-slider__click-area")
+        ],
         max: 1000000,
         min: 30000,
-        currentPosition: 30000
+        step: 1000,
+        currentPosition: 30000,
+        throttledDispatch: 100
     });
 
     var calculator = new DepositCalculator({
@@ -220,7 +276,11 @@
         slider: calculatorSlider,
         depositElement: calculatorElement.querySelector(".d-c-result__deposit"),
         resultElement: calculatorElement.querySelector(".d-c-result__final-summ"),
-        bubbleElement: calculatorElement.querySelector(".d-c-slider__bubble")
+        bubbleElement: calculatorElement.querySelector(".d-c-slider__bubble"),
+        buttonsContainer: calculatorElement.querySelector(".d-c-period-button-container"),
+        periods: [1, 3, 6],
+        yearProfit: 6.4,
+        periodName: "мес"
     });
 
 })(window);
